3ab1e717a012867ddc0491b546ea2267b0f18893
Zachary Johnson <zjohnson@frontlineed.com>
2019-02-11 09:21:53 -0600

    Initial
    Initial

---
+++ src/components/BookList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                default: 'BookList'
+            },
+             title: {
+                type: String,
+                default: 'All Books' 
+            },
+        },
+        methods: {
+            data() {
+                return title;
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    h1,h2{
+        font-weight:normal
+    }
+</style>
