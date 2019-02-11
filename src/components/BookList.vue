e9fe633bd9140a6521ed28b7313e16149c9b6517
Zachary Johnson <zjohnson@frontlineed.com>
2019-02-11 09:41:31 -0600

    3
    3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,18 +1,38 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <ul>
+            <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
         name:'BookList',
-        data(){return{title:'All Books'}}
+        data(){
+            return{
+                title:'All Books',
+                books: [
+                    {title: 'Title 1', author:'Author 1'},
+                    {title: 'Title 2', author:'Author 2'},
+                    {title: 'Title 3', author:'Author 3'},
+                    ]
+            }
+        }
     }
 </script>
 
 <style scoped>
     h1,h2{
         font-weight:normal
+    }
+    ul{
+        list-style-type:none;
+        padding:0;
+    }
+    li{
+        display:block;
+        margin:0 10px;
     }
 </style>
